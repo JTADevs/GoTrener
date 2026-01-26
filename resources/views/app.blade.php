adab4ef2b367e6e21d6cbb49b36535e99fa56f34
Sebastian Skubala <sebsqu@gmail.com>
2026-01-26 18:02:18 +0100

    home fix visual aspects

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,6 +2,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="theme-color" content="#241F20">
         <link 
         rel="stylesheet" 
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" 
